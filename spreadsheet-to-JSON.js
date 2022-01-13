6d76c21135f22c9e6427a1c866fe27a2e2c9bf77
Randy Lubin <randylubin@gmail.com>
2022-01-12 18:04:21 -0800

    updated and streamlined CSV-JSON script

--- spreadsheet-to-JSON.js
+++ spreadsheet-to-JSON.js
@@ -5,56 +5,76 @@ const fs = require('fs');
 let rawdata = fs.readFileSync('spreadsheet-input.json');
 let sheetData = JSON.parse(rawdata);
 
-let dataOutput = []
+let titleCol = 1
+let promptCol = 2
+let optionCol = 3
+let resultsCol = 4
+let requirementsCol = 5
+let visibilityCol = 6
+let capitalCol = 7
+let userCol = 8
+let capabilitiesCol = 9
+let otherStateCol = 10
+let gameOverCol = 11
+let engineIssueCol = 12
+
+let dataOutput = {}
 
 for (const dataRow in sheetData) {
   let row = sheetData[dataRow]
   if (!dataOutput[row[0]]) {
     dataOutput[row[0]] = {
-      dilemmaPrompt: row[1],
+      dilemmaTitle: row[titleCol],
+      dilemmaPrompt: row[promptCol],
       dilemmaOptions: [],
+      engineIssue: row[engineIssueCol]
     }
   }
 
   let newOption = {
-    optionText: row[2],
-    resultsText: row[3],
+    optionText: row[optionCol],
+    resultsText: row[resultsCol],
     optionRequirements: {},
     optionVisibility: {},
-    stateChange: {},
-    gameOver: row[7] ?? null,  
+    stateChange: {
+      capital: 0,
+      users: 0,
+      capabilities: 0
+    },
+    gameOver: row[gameOverCol] ?? null,  
   }
 
   // Option Requirements
-  if (row[4]) {
-    let newRequirements = row[4].split(',')
-    for (const req in newRequirements) {
-      let newReqObject = newRequirements[req].split(':')
-      console.log('newReqObject', newReqObject)
+  if (row[requirementsCol]) {
+    let newRequirements = row[requirementsCol].split(',')
+    for (const obj in newRequirements) {
+      let newReqObject = newRequirements[obj].split(':')
       newOption.optionRequirements[newReqObject[0]] = newReqObject[1]
     }  
   } else {newOption.optionRequirements = null}
 
   // Option visibility
-  if (row[5]) {
-    let newRequirements = row[5].split(',')
-    for (const req in newRequirements) {
-      let newReqObject = newRequirements[req].split(':')
-      console.log('newReqObject', newReqObject)
-      newOption.optionVisibility[newReqObject[0]] = newReqObject[1]
+  if (row[visibilityCol]) {
+    let newVisSection = row[visibilityCol].split(',')
+    for (const obj in newVisSection) {
+      let newVisObject = newVisSection[obj].split(':')
+      newOption.optionVisibility[newVisObject[0]] = newVisObject[1]
     }  
   } else {newOption.optionVisibility = null}
 
   // State Change
-  if (row[6]) {
-    let newRequirements = row[6].split(',')
-    for (const req in newRequirements) {
-      let newReqObject = newRequirements[req].split(':')
-      console.log('newReqObject', newReqObject)
-      newOption.stateChange[newReqObject[0]] = newReqObject[1]
-    }  
-  } else {newOption.stateChange = null}
+  newOption.stateChange['capital'] = row[capitalCol] ? parseInt(row[capitalCol]) : 0
+  newOption.stateChange['users'] = row[userCol] ? parseInt(row[userCol]) : 0
+  newOption.stateChange['capabilities'] = row[capabilitiesCol] ? parseInt(row[capabilitiesCol]) : 0
 
+  
+  if (row[otherStateCol]) {
+    let newStateSection = row[otherStateCol].split(',')
+    for (const obj in newStateSection) {
+      let newStateObj = newStateSection[obj].split(':')
+      newOption.stateChange[newStateObj[0]] = newStateObj[0] == "focus" ? parseInt(newStateObj[1]) : newStateObj[1]
+    }  
+  }
 
   dataOutput[row[0]].dilemmaOptions.push(newOption)
 }
--- src/components/DilemmaCompiler.js
+++ src/components/DilemmaCompiler.js
@@ -1,220 +1,63 @@
+import DilemmaList from '../../spreadsheet-output.json'
+
 var DilemmaCompiler = [
    // location
    function () {
-      return {            
-            "dilemmaPrompt":"Your first decision is to pick a location for your startup. Some areas are more expensive than others, and your choice will impact future access to capital, talent, and internet connectivity.",
-            "dilemmaOptions":[
-            {
-            "optionText":"Tech Hub – expensive but with world class talent and tons of investors",
-            "resultsText":"This is the place to be, and even if that co-working space only gives us a desk in the corner for 3x the cost of a full office somewhere else, we’ll be seen as a *real* startup now. ",
-            "optionRequirements":null,
-            "optionVisibility":null,
-            "stateChange":{
-            "techHub":" true"
-            },
-            "gameOver":""
-            },
-            {
-            "optionText":"Other City – an aspiring tech hub with a lower cost of living and a smaller talent pool",
-            "resultsText":"Silicon Valley is so last century. Innovation is about newness, and the Crypto Hills is where everyone is heading these days.",
-            "optionRequirements":null,
-            "optionVisibility":null,
-            "stateChange":{
-            "otherCity":" true"
-            },
-            "gameOver":""
-            },
-            {
-            "optionText":"College Town – home of a prestigious university and brimming with talent, though lacking in investors",
-            "resultsText":"Startups are kind of like a post-college experience anyway, might as well set up shop in a college town. Plus, so many eager CS students! ",
-            "optionRequirements":null,
-            "optionVisibility":null,
-            "stateChange":{
-            "collegeTown":" true"
-            },
-            "gameOver":""
-            },
-            {
-            "optionText":"Small Town – it’s cheap but there’s not much in the way of local investors and tech talent",
-            "resultsText":"The world is remote these days anyway – let’s not waste time and money on expensive offices. Life here is bucolic and we can quickly become the biggest company in town.",
-            "optionRequirements":null,
-            "optionVisibility":null,
-            "stateChange":{
-            "smallTown":" true"
-            },
-            "gameOver":""
-            }
-            ]
-      }
-
+      return DilemmaList['1']
    },
    // First focus
    function () {
-      return {
-         "dilemmaPrompt": "As a founder, your time is incredibly valuable and there are endless opportunities on which you can focus. Working on your core product is always a good idea, but it might be worth taking some time away from that to strengthen your company in other ways. ",
-         "dilemmaOptions": [
-            {
-               "optionText": "Start networking with investors to prepare for your Series A fundraising later this year [Focus]",
-               "resultsText": "Why do I keep getting handed off to Associates?",
-               "stateChange": {
-                  "investorNetwork": true,
-                  "focus": -1,
-               },
-				"optionRequirements": {
-					"focus": 1,
-               },
-            },
-            {
-               "optionText": "Focus on recruiting and building an amazing team [Focus]",
-               "resultsText": "We've got core values AND a foosball table",
-               "stateChange": {
-                  "hiringFocus": true,
-                  "focus": -1,
-               },
-				"optionRequirements": {
-                  "focus": 1
-               },
-            },
-            {
-               "optionText": "Ignore other distractions and stay focused on building the best product",
-               "resultsText": "More user interviews!",
-               "stateChange": {
-                  "productFocus": true,
-               },
-            },
-         ]
-      }
+      return DilemmaList['2']
    },
    // template
    function (gameState) {
       if (gameState.techHub) {
-         return {
-            "dilemmaPrompt": "What are you prioritizing in your recruiting?",
-            // The best candidates, regardless of their cost or if they need to work remotely
-            // [locked unless focusing on recruiting] Tap your local network, you’ve been meeting plenty of great folks here and in-person collaboration is critical
-            // We’re going to hire smart but inexperienced people and train them
-            // We’ll outsource as much of the work as possible to an inexpensive team overseas
-            "dilemmaOptions": [
-               {
-                  "optionText": "The best candidates, regardless of their cost or if they need to work remotely",
-                  "resultsText": "Placeholder",
-                  "stateChange": {
-                     "capital": -25,
-                     "capabilities": 25,
-                  }, 
-               },
-               {
-                  "optionText": "Tap your local network, you’ve been meeting plenty of great folks here and in-person collaboration is critical",
-                  "resultsText": "Placeholder",
-                  "optionRequirements": {
-                     "hiringFocus": true
-                  },
-                  "stateChange": {
-                     "capital": -10,
-                     "capabilities": 20,
-                  }, 
-               },
-               {
-                  "optionText": "We’re going to hire smart but inexperienced people and train them",
-                  "resultsText": "Placeholder",
-                  "stateChange": {
-                     "capital": -10,
-                     "capabilities": 10,
-                  }, 
-               },
-               {
-                  "optionText": "We’ll outsource as much of the work as possible to an inexpensive team overseas",
-                  "resultsText": "Placeholder",
-                  "stateChange": {
-                     "capital": -5,
-                     "capabilities": 5,
-                  }, 
-               },
-            ]
-         }
+         return DilemmaList['3.1']
       } else {
-         return {
-         // You’ve found a brilliant engineer who would be a great fit for your company - but they don’t live anywhere near your headquarters. What do you want to do?
-         // Set them up to work remotely
-         // Offer to pay for their relocation
-         // [locked unless focusing on recruiting] Find someone closer who’s just as good
-
-         "dilemmaPrompt": "You’ve found a brilliant engineer who would be a great fit for your company - but they don’t live anywhere near your headquarters. What do you want to do?",
-         "dilemmaOptions": [
-            {
-               "optionText": "Set them up to work remotely",
-               "resultsText": "Placeholder result",
-               "stateChange": {
-                  "capital": -5,
-               },
-            },
-            {
-               "optionText": "Offer to pay for their relocation",
-               "resultsText": "Placeholder result",
-               "stateChange": {
-                  "capital": -15,
-               },
-            },
-            {
-               "optionText": "Find someone closer who’s just as good",
-               "resultsText": "Placeholder",
-               "optionRequirements": {
-                  "hiringFocus": true
-               },
-               "stateChange": {
-                  "capital": -5,
-               }, 
-            },
-         ]
-         }
+         return DilemmaList['3.2']
       }
    },
-   // privacy
-   // An engineer comes to you concerned about the large amounts of user data you are collecting and retaining, including sensitive personal information. How do you want to proceed?
-   // Invest in higher quality security so that you can safely continue leveraging the user data to make product decisions and deliver targeted ads --💰 +👥
-   // Cut down on collected data because user privacy is highly important
-   // Let’s dig into the user data to enhance the product and user experience [BadPrivacy] ++👥 -⏱
-   // Deprioritize – there are way more important features for us to develop [BadPrivacy]
-
    function () {
-      return {
-         "dilemmaPrompt": "An engineer comes to you concerned about the large amounts of user data you are collecting and retaining, including sensitive personal information. How do you want to proceed?",
-         "dilemmaOptions": [
-            {
-               "optionText": "Invest in higher quality security so that you can safely continue leveraging the user data to make product decisions and deliver targeted ads",
-               "resultsText": "This is the placeholder result of an option",
-               "stateChange": {
-                  "capital": -5,
-                  "goodSecurity": true
-               },
-            },
-            {
-               "optionText": "Cut down on collected data because user privacy is highly important",
-               "resultsText": "This is the placeholder result of an option",
-               "stateChange": {
-                  "capital": -1,
-               },
-            },
-            {
-               "optionText": "Let’s dig into the user data to enhance the product and user experience [Focus]",
-               "resultsText": "This is the placeholder result of an option",
-               "stateChange": {
-                  "badPrivacy": true,
-                  "users": +5
-               },
-               "optionRequirements": {
-                  "focus": 1
-               },
-            },
-            {
-               "optionText": "Deprioritize – there are way more important features for us to develop",
-               "resultsText": "This is the placeholder result of an option",
-               "stateChange": {
-                  "badPrivacy": true
-               },
-            },
-         ]
-      }
+      return DilemmaList['4']
+   },
+   function () {
+      return DilemmaList['5']
+   },
+   function () {
+      return DilemmaList['6']
+   },
+   function () {
+      return DilemmaList['7']
+   },
+   function () {
+      return DilemmaList['8']
+   },
+   function () {
+      return DilemmaList['9']
+   },
+   function () {
+      return DilemmaList['10']
+   },
+   function () {
+      return DilemmaList['11']
+   },
+   function () {
+      return DilemmaList['12']
+   },
+   function () {
+      return DilemmaList['13']
+   },
+   function () {
+      return DilemmaList['14']
+   },
+   function () {
+      return DilemmaList['15']
+   },
+   function () {
+      return DilemmaList['16']
+   },
+   function () {
+      return DilemmaList['17']
    },
    // example dilemma
    // function () {
