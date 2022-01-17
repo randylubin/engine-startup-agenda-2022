fedec75eb330d8d5235cadb2fe3ec907134a4b88
Randy Lubin <randylubin@gmail.com>
2022-01-17 09:11:27 -0800

    debug console working

--- src/components/ChapterControlPanel.vue
+++ src/components/ChapterControlPanel.vue
@@ -1,7 +1,24 @@
 <template>
- <div id="chapter-control-panel">
-    <button v-on:click="restartGame()"><span>Restart</span></button>
-    <button v-on:click="undoChoice()" :disabled="chapterHistory.length == 1"><span>Previous</span></button>
+  <div>
+    <div id="chapter-control-panel">
+      <button v-on:click="restartGame()"><span>Restart</span></button>
+      <button v-on:click="undoChoice()" :disabled="chapterHistory.length == 1"><span>Previous</span></button>
+      <button v-on:click="toggleDebug()"><span>Debug</span></button>
+    </div>
+    <div id="debug-panel" v-if="debugOpen">
+      <h1 class="text-center">Debug Options</h1>
+      <div class="state-editor">
+        <textarea v-model="editedStateString" cols="30" rows="10"></textarea>
+        <div>{{jsonError}}</div>
+        <button v-on:click="updateState(editedStateString)" :disabled="currentStateString == editedStateString || jsonError">Update</button>
+      </div>
+      <div class="go-to-chapter">
+        Go to chapter: <input v-model="targetChapter" type="text" id="input-go-to-chapter"> <button v-on:click="goToChapter(targetChapter)" :disabled="!targetChapter">Go</button>
+      </div>
+      <button v-on:click="toggleDebug()">Close</button>
+
+
+    </div>
   </div>
 </template>
 
@@ -10,15 +27,34 @@
   export default {
     name: 'chapter-control-panel',
     props: {
-      chapterHistory: Array
+      chapterHistory: Array,
+      currentStateString: String,
     },
     components: {
     },
     data () {
       return {
+        debugOpen: false,
+        targetChapter: null,
+        editedStateString: null, 
       }
     },
     mounted () {
+    },
+    computed: {
+      jsonError: function(){
+        try {
+          JSON.parse(this.editedStateString)
+          return null
+        } catch(e) {
+          return e;
+        }
+      }
+    },
+    watch: {
+      currentStateString: function(state){
+        this.editedStateString = state;
+      }
     },
     methods: {
       restartGame () {
@@ -27,11 +63,22 @@
       undoChoice(){
         this.$emit('undo-state-change')
       },
+      toggleDebug(){
+        this.debugOpen = !this.debugOpen
+      },
+      goToChapter(targetChapter){
+        this.$emit('go-to-chapter', targetChapter);
+        this.toggleDebug();
+        this.targetChapter = null;
+      },
+      updateState(newStateString){
+        this.$emit('update-state', JSON.parse(newStateString));
+      }
     }
   }
 </script>
 
-<style>
+<style lang="scss" >
 
 div#chapter-control-panel {
 	display: flex;
@@ -54,5 +101,29 @@ div#chapter-control-panel button {
 }
 
 div#chapter-control-panel button::before { content: '' }
+
+div#debug-panel {
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 5001;
+  background: black;
+  width: 500px;
+  padding: 1em;
+
+  button {
+    width: auto;
+  }
+}
+
+.go-to-chapter {
+  display: inline;
+}
+
+#input-go-to-chapter {
+  width: 35px;
+  margin: 0px 10px;
+  display: inline;
+}
 
 </style>
