6d94201a92c6391ef226dd05ba9d1563e205cc92
Leigh <leigh.beadon@gmail.com>
2022-04-26 17:35:05 -0400

    debug tools label

--- src/components/GameDevtools.vue
+++ src/components/GameDevtools.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <div id="devtools-panel">
-      <button v-on:click="restartGame()"><span>Restart</span></button>
-      <button v-on:click="undoChoice()" :disabled="chapterHistory.length == 1"><span>Previous</span></button>
-      <button v-on:click="toggleDebug()"><span>Debug</span></button>
+      <p>Debug Tools (Shift-D to close)</p>
+      <div>
+        <button v-on:click="restartGame()"><span>Restart</span></button>
+        <button v-on:click="undoChoice()" :disabled="chapterHistory.length == 1"><span>Back</span></button>
+        <button v-on:click="toggleDebug()"><span>Debug</span></button>
+      </div>
     </div>
     <div id="debug-panel" v-if="debugOpen">
       <h1 class="text-center">Debug Options</h1>
@@ -89,26 +92,40 @@
 <style lang="scss" >
 
 div#devtools-panel {
-	display: flex;
 	position: fixed;
 	left: 0; bottom: 0;
-	padding: .5em;
+	padding: .25em .5em .5em;
 	background: #000;
 	z-index: 5000;
 }
 
+div#devtools-panel > p {
+  font-family: acumin-pro-condensed, sans-serif; 
+  font-size: .6em;
+  font-weight: bold;
+  text-transform: uppercase;
+  text-align: center;
+  color: #aaa;
+  margin: 0 0 .5em 0;
+}
+
+div#devtools-panel > div {
+  display: flex;
+}
+
 div#devtools-panel button {
+  text-align: center;
+  color: #aaa;
 	column-gap: .25em;
 	font-size: .75em;
-	padding: .25em;
-	padding-right: 1em;
+	padding: .25em 1em .25em .25em;
 	margin-right: .5em;
 	margin-bottom: 0;
-	
+	box-shadow: none;
 	background: var(--bg-button-disabled);
 }
 
-div#devtools-panel button::before { content: '' }
+div#devtools-panel button::before { content: ''; width: 0;}
 
 div#debug-panel {
   position: absolute;
