0511787d000020b0d1f0781be87699344f3a7418
Leigh <leigh.beadon@gmail.com>
2022-04-26 21:48:09 -0400

    more state flags

--- src/components/FlagIndex.js
+++ src/components/FlagIndex.js
@@ -27,10 +27,10 @@ var FlagIndex = {
 		trueText: "you are focused on meeting investors",
 		falseText: "you don't have a strong enough VC network"
 	},
-	"fightTroll": {
+	"foughtTroll": {
 		flagTitle: "Fighting a Troll",
 		flagClass: "focus",
-		flagText: "You're engaged in a legal battle with a patent troll",
+		flagText: "You're engaged in a legal battle with a patent troll.",
 		trueText: "you are fighting a Patent Troll",
 		falseText: "you aren't fighting the Patent Troll"
 	},
@@ -38,6 +38,61 @@ var FlagIndex = {
 		trueText: "you had previously built a bespoke content identificaiton system",
 		falseText: "you don't have a functioning content identificaiton system"
 	},
+	"buildingContentID": {
+		flagTitle: "Building Copyright Tech",
+		flagClass: "focus",
+		flagText: "You're working on a custom system for dealing with pirated content."
+	},
+	"hiringCopymods": {
+		flagTitle: "Dealing With Takedowns",
+		flagText: "You're struggling to hire enough moderators to keep up with copyright takedowns."
+	},
+	"NSLFight": {
+		flagTitle: "Fighting an NSL",
+		flagClass: "focus",
+		flagText: "You're fighting against a National Security Letter."
+	},
+	"movingHQ": {
+		flagTitle: "Moving Headquarters",
+		flagText: "You're settling in at your new headquarters."
+	},
+	"legalThreats": {
+		flagTitle: "Legal Threats",
+		flagText: "You've been fielding ongoing legal threats from an angry newscaster."
+	},
+	"breachLawsuit": {
+		flagTitle: "Data Breach Lawsuit",
+		flagText: "You're facing a lawsuit over your handling of a data breach."
+	},
+	"moderatingContent": {
+		flagTitle: "Content Moderation",
+		flagText: "You're working to fine-tune your content moderation rules and practices."
+	},
+	"moderationComplaints": {
+		flagTitle: "Algorithm Complaints",
+		flagText: "You're fielding a lot of complaints about your content moderation algorithm."
+	},
+	"expertReview": {
+		flagTitle: "Disinformation Review",
+		flagText: "You're dealing with judgement calls from your expert disinformation board."
+	},
+	"angryAdvertisers": {
+		flagTitle: "Damage Control",
+		flagText: "You're trying to hang on to advertisers as people target them over your disinformation policies."
+	},
+	"banDisinfo": {
+		flagTitle: "Damage Control",
+		flagText: "You're facing accusations of censorship over your disinformation policies."
+	},
+	"buildingDisinfo": {
+		flagTitle: "Dealing With Disinfo",
+		flagText: "You're building a system for flagging disinformation with additional sources."
+	},
+	"indiaSubsidiary": {
+		flagTitle: "Setting Up In India",
+		flagClass: "focus",
+		flagText: "You're working on setting up a subsidiary in India to comply with data regulations."
+	}
 };
 
 export default FlagIndex;
